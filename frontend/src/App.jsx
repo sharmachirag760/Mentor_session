72c13f3fdb105967f12f6d4552482e6345c11696
Chirag Sharma <chirag@Chirags-MacBook-Air.local>
2025-06-22 15:35:28 +0530

    hero section completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './page'
 function App() {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+       <Route path='/' element={<Home/>}/>
+    </Routes>
+    
+    </BrowserRouter>
 
     </>
   )
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        "screen-3xl": "1920px",
+      },
+      colors: {
+        'vibrant-green': '#2CC84D',
+        'orange-color': '#FFDD40',
+        'gray-color': '#fef4d1',
+        'button-color':'#fec200',
+      },
+    },
+  },
+  plugins: [
+    tailwindcss(),
+    react()],
 })
